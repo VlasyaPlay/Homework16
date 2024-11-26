0ed3c065f2aec9f119869125f297c9808b6e880c
VlasyaPlay <alexvlasov9301@gmail.com>
2024-11-26 12:24:55 +0700

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,20 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+  return ("Главная страница")
+
+@app.get("/user/admin")
+async def admin():
+  return ("Вы вошли как администратор")
+
+@app.get("/user/{user_id}")
+async def users_id(user_id: str):
+  return (f"Вы вошли как пользоваетль №{user_id}")
+
+@app.get("/id")
+async def new_user(username: str, age: int):
+  return (f"Информация о пользователе. Имя: {username}, Возраст: {age}")
